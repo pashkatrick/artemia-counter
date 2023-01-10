324113454bc02aab029ca6e362ff3244e4828a9b
DariAcuna <dacunaq@uni.pe>
2023-01-10 17:33:11 -0500

    fix page4 css rendering

--- PRUEBA_IMPORT.py
+++ PRUEBA_IMPORT.py
@@ -1,10 +1,142 @@
 from OBJETO_ARTEMIAS import motor    
 from time import sleep
 import cv2 as cv
+import numpy as np
 import time
 
-def matrix_motion (tipo_pozo,tiempo_foto,check_pozo, cam):
+camera = cv.VideoCapture(0)
+camera.set(cv.CAP_PROP_FRAME_WIDTH, 640) #2028
+camera.set(cv.CAP_PROP_FRAME_HEIGHT, 480) #1520
+camera.set(cv.CAP_PROP_FPS, 30)
+
+save = 0
+art_num = 0
+art = []
+
+#def capture():
+#    if camera.isOpened():
+#        ret, img = camera.read()
+#        if ret:
+#            cv.imwrite('./images/img', img)
+#            return img
+
+def count(img):
+
+    # Find circle
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    blurred = cv.medianBlur(gray, 25)  # cv2.bilateralFilter(gray,10,50,50)
+
+    # these work for 640 x 360
+    minDist = 100
+    param1 = 50
+    param2 = 30     # smaller value -> more false circles
+    minRadius = 120
+    maxRadius = 300
+
+    # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
+    circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2,
+                               minRadius=minRadius, maxRadius=maxRadius)
+
+    xc = 0
+    yc = 0
+    radius = 0
+
+    roi = img.copy()
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            cv.circle(roi, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            xc = i[0]
+            yc = i[1]
+            radius = round(i[2] * 0.98)
+    else:
+        return 0
+
+    # draw filled circle in white on black background as mask
+    mask = np.zeros_like(img)
+    mask = cv.circle(mask, (xc, yc), radius, (255, 255, 255), -1)
+    result = cv.bitwise_and(img, mask)
+
+    if yc - radius < 0 or 360 < yc + radius:
+        return 0
+    if xc - radius < 0 or 640 < xc + radius:
+        return 0
+    crop = result[yc - radius:yc + radius, xc - radius:xc + radius]
+
+    # MAKE CASE FOR WHEN CIRCLE IS NOT DETECTED
+
+    hsv = cv.cvtColor(crop, cv.COLOR_BGR2HSV)
+    # (0, 43, 75), (34, 255, 255)
+    artemia = cv.inRange(hsv, (0, 37, 75), (54, 255, 161))
+
+    back = crop.copy()
+    back[artemia == 255] = (0, 255, 0)
+
+    back[artemia != 255] = (0, 0, 0)
+
+    kernel = np.ones((4, 4), np.uint8)
+    dil = cv.dilate(back, kernel, iterations=1)
+
+    gray2 = cv.cvtColor(dil, cv.COLOR_BGR2GRAY)
+    th, threshed = cv.threshold(gray2, 0, 255, cv.THRESH_BINARY)
+
+    cnts = cv.findContours(threshed, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[-2]
+
+    s1 = 40
+    s2 = 200   # 100 x 100 for (1920 x 1080)
+    xcnts = []
+    for cnt in cnts:
+        # if s1 < cv2.contourArea(cnt) < s2:
+        area = cv.contourArea(cnt)
+        peri = cv.arcLength(cnt, True)
+        circularity = 4 * np.pi * (area / peri**2)
+        if 0.7 < circularity < 1:
+            if s1 < cv.contourArea(cnt) < s2:
+                xcnts.append(cnt)
+
+    return len(xcnts)
+
+def gen_frames():
+    global art_num
+    global art
+    global save
     
+    while True:
+        success, frame = camera.read()
+        frame = cv.resize(frame, (640, 360), interpolation=cv.INTER_AREA)
+        
+        if success:
+            
+            if (save):
+                # cv.imwrite('./test_final_2.jpg', frame)
+                art_num = count(frame)
+                art.append(art_num)
+                
+                save = 0
+            #    save = 0
+            #    fileName = "img11.jpg"
+            #    cmd = "raspistill -o " + fileName
+            #    subprocess.call(cmd, shell=True)
+            #    print('saving')
+            #    sleep(4)
+            #    print('saved')
+            
+            try:
+                ret, buffer = cv.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+        else:
+            pass
+
+def matrix_motion (tipo_pozo,tiempo_foto,check_pozo):
+    global save
+    global art_num
+    global art
     # check_pozo:
     # array de bools de los pozos
     # 0: no seleccionado (no se toma foto)
@@ -31,12 +163,12 @@ def matrix_motion (tipo_pozo,tiempo_foto,check_pozo, cam):
     elif tipo_pozo == 2:
         dis_init_x = 0.5
         dis_init_y = -0.3
-        num_x = 6
-        num_y = 4
+        num_x = 4
+        num_y = 6
         dist_pozos = 1.93
     elif tipo_pozo == 3:
-        dis_init_x = 0.5
-        dis_init_y = -0.275 #-0.3?
+        dis_init_x = 0.75
+        dis_init_y = -0.3 #-0.275?
         num_x = 4
         num_y = 6
         dist_pozos = 1.93
@@ -64,7 +196,7 @@ def matrix_motion (tipo_pozo,tiempo_foto,check_pozo, cam):
     i = 0
     j = 0
     
-    images = [None] * (num_x * num_y)
+    # images = [None] * (num_x * num_y)
     
     while j < num_y:
         mot_y.run_position(-j*dist_pozos + dis_init_y)
@@ -84,13 +216,22 @@ def matrix_motion (tipo_pozo,tiempo_foto,check_pozo, cam):
                 print("tomando foto: ",i+1," ",j+1)
                 
                 # cmd: take picture
-                success, frame = cam.read()
-                images[num_x*(j)+i] = frame
+                # success, frame = cam.read()
+                # images[num_x*(j)+i] = frame
+                #image_1 = capture()
+                #count_1 = count(image_1)
+                
+                # to avoid motion blur and let the camera locate properly
+                time.sleep(0.5)
+                save = 1
+                
+                #print('Artemias: ', count_1)
+                #cv.imwrite('./images/final_test.jpg')
                 
                 time.sleep(tiempo_foto)
             
-            else:
-                images[num_x*(j)+i] = None
+            # else:
+                # images[num_x*(j)+i] = None
             
             i = i + 1
         
@@ -100,7 +241,7 @@ def matrix_motion (tipo_pozo,tiempo_foto,check_pozo, cam):
     mot_x.set_motor()
     mot_y.set_motor()
     
-    return images
+    return art
 
 #matrix = [1,0,0,0,0,0] 
 #matrix_motion(1,5,matrix)
--- flask-server.py
+++ flask-server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, render_template, request, make_response, send_file
 #from picamera2.picamera2 import *
 from PRUEBA_IMPORT import matrix_motion
+from PRUEBA_IMPORT import gen_frames
 from time import sleep
 import numpy as np
 import cv2 as cv
@@ -12,113 +13,16 @@ app = Flask(__name__,
             static_folder='./web/static',
             template_folder='./web/templates')
 
-camera = cv.VideoCapture(0)
-camera.set(cv.CAP_PROP_FRAME_WIDTH, 640) #2028
-camera.set(cv.CAP_PROP_FRAME_HEIGHT, 480) #1520
-camera.set(cv.CAP_PROP_FPS, 30)
+#camera = cv.VideoCapture(0)
+#camera.set(cv.CAP_PROP_FRAME_WIDTH, 640) #2028
+#camera.set(cv.CAP_PROP_FRAME_HEIGHT, 480) #1520
+#camera.set(cv.CAP_PROP_FPS, 30)
 
 save = 0
+result = []
 
 dir = "/home/pi/Biomet/images/"
 
-def count(img):
-
-    # Find circle
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-    blurred = cv.medianBlur(gray, 25)  # cv2.bilateralFilter(gray,10,50,50)
-
-    # these work for 640 x 360
-    minDist = 100
-    param1 = 50
-    param2 = 30     # smaller value -> more false circles
-    minRadius = 120
-    maxRadius = 300
-
-    # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
-    circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2,
-                               minRadius=minRadius, maxRadius=maxRadius)
-
-    xc = 0
-    yc = 0
-    radius = 0
-
-    roi = img.copy()
-
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0, :]:
-            cv.circle(roi, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            xc = i[0]
-            yc = i[1]
-            radius = round(i[2] * 0.98)
-
-    # draw filled circle in white on black background as mask
-    mask = np.zeros_like(img)
-    mask = cv.circle(mask, (xc, yc), radius, (255, 255, 255), -1)
-    result = cv.bitwise_and(img, mask)
-
-
-    crop = result[yc - radius:yc + radius, xc - radius:xc + radius]
-
-    hsv = cv.cvtColor(crop, cv.COLOR_BGR2HSV)
-    # (0, 43, 75), (34, 255, 255)
-    artemia = cv.inRange(hsv, (0, 37, 75), (54, 255, 161))
-
-    back = crop.copy()
-    back[artemia == 255] = (0, 255, 0)
-
-    back[artemia != 255] = (0, 0, 0)
-
-    kernel = np.ones((4, 4), np.uint8)
-    dil = cv.dilate(back, kernel, iterations=1)
-
-    gray2 = cv.cvtColor(dil, cv.COLOR_BGR2GRAY)
-    th, threshed = cv.threshold(gray2, 0, 255, cv.THRESH_BINARY)
-
-    cnts = cv.findContours(threshed, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[-2]
-
-    s1 = 40
-    s2 = 200   # 100 x 100 for (1920 x 1080)
-    xcnts = []
-    for cnt in cnts:
-        # if s1 < cv2.contourArea(cnt) < s2:
-        area = cv.contourArea(cnt)
-        peri = cv.arcLength(cnt, True)
-        circularity = 4 * np.pi * (area / peri**2)
-        if 0.7 < circularity < 1:
-            if s1 < cv.contourArea(cnt) < s2:
-                xcnts.append(cnt)
-
-    return len(xcnts)
-
-def gen_frames():
-    global save
-    
-    while True:
-        success, frame = camera.read()
-        frame = cv.resize(frame, (640, 360), interpolation=cv.INTER_AREA)
-        
-        if success:
-            
-            #if (save):
-            #    save = 0
-            #    fileName = "img11.jpg"
-            #    cmd = "raspistill -o " + fileName
-            #    subprocess.call(cmd, shell=True)
-            #    print('saving')
-            #    sleep(4)
-            #    print('saved')
-            
-            try:
-                ret, buffer = cv.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
-        else:
-            pass
 
 @app.route('/requests', methods=['POST','GET'])
 def tasks():
@@ -174,15 +78,18 @@ def index2():
 
 @app.route('/page3', methods=['GET', 'POST'])
 def index3():
-    
+    global result
     if request.method == 'GET':
         return render_template('page3.html')
     else:
         if my_var == 1:
+            x = 2
             y = 3
         elif my_var == 2:
+            x = 3
             y = 4
         elif my_var == 3:
+            x = 4
             y = 6
         bools = request.get_json(force=True)
         pre = [int(e) for e in bools]
@@ -190,19 +97,32 @@ def index3():
         B = np.flip(A, axis=1)
         C = np.transpose(B)
         D = C.flatten()
-        images = matrix_motion(my_var,2,D,camera)
+        counts = matrix_motion(my_var,3,D)
+        counts = np.array(counts)
+        indices = np.nonzero(D)[0][:len(counts)]
+        result = np.zeros_like(D)
+        np.put(result, indices, np.take(counts, np.arange(len(indices))))
+        result = np.transpose(result.reshape(x,y))
+        result = result.ravel()
         
         # IMAGES NEED TO MATCH LOCATIONS
         
-        counts = []
         # use [A1, A2, ] matrix thing here
-        for img in images:
-            if img is not None:
-                counts.append(count(img))
-            else:
-                counts.append(None)
+        # for img in images:
+        #    if img is not None:
+        #        counts.append(count(img))
+        #    else:
+        #        counts.append(None)
         
-        return render_template('page3.html', D=D, counts=counts, dim=dim)
+        # counts=counts
+        return render_template('page3.html')
+    
+@app.route('/pag4')
+def index4():
+    global dim
+    global result
+    if request.method == 'GET':
+        return render_template('page4.html', result=result, dim=dim)
 
 @app.route('/test')
 def test():
